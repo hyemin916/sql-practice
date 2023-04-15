3dbd10860c0eaa284591c2c857bb50cffd3dede5
박혜민 <hyemin@ktown4u.com>
2023-04-15 10:33:08 +0900

    use sub query

--- 1.sql
+++ 1.sql
@@ -1,3 +1,7 @@
-SELECT * FROM dept_emp
-WHERE dept_no IN ('d003', 'd004', 'd005')
-  AND emp_no=10001;
+SELECT *
+FROM dept_emp USE INDEX(PRIMARY)
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dpartments
+    WHERE dept_no BETWEEN 'd003' AND 'd005')
+AND emp_no=10001;
